f17140baadd799617fef50c44e25c08b5dc65127
Daryna <darynapoltavchenko@gmail.com>
2024-02-09 18:05:58 +0100

    feat: calculation step first done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -14,6 +14,34 @@ $(document).ready(function(){
         ]
 
     });
+
+    mobileOnlySlider("#slider-advantages", true, false, 1024);
+
+    function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+      var slider = $($slidername);
+      var settings = {
+        mobileFirst: true,
+        dots: $dots,
+        arrows: $arrows,
+        responsive: [
+          {
+            breakpoint: $breakpoint,
+            settings: "unslick"
+          }
+        ]
+      };
+    
+      slider.slick(settings);
+    
+      $(window).on("resize", function () {
+        if ($(window).width() > $breakpoint) {
+          return;
+        }
+        if (!slider.hasClass("slick-initialized")) {
+          return slider.slick(settings);
+        }
+      });
+    } // Mobile Only Slider
   
 
     const burger = document.querySelector('.btn-burger');
@@ -27,4 +55,40 @@ $(document).ready(function(){
     }
 
     burger.addEventListener('click', toggleMenu)
+
+    const fileInput = document.querySelector('#fileInput');
+    const fileName = document.querySelector('#file-name')
+
+    fileInput.addEventListener('change', function() {
+      //fileName.innerHTML = fileInput.value;    long name
+      fileName.innerHTML = this.files[0].name; //short name
+    })
+
+    const typeTheckbox = document.querySelector('[type=checkbox]');
+
+    typeTheckbox.closest('.input-column').classList.add('column-checkbox');
+
+    const checkbox = document.querySelectorAll('[type="checkbox"]')
+
+    console.log(checkbox);
+    if(checkboxes.length > 0) {
+        checkbox.forEach(test => {
+            test.closest('.input-column').classList.add('column-checkbox')
+        })
+    }
+
+    const file = document.querySelectorAll('.input-file')
+
+    if(file.length > 0) {
+        file.forEach(item => {
+        item.closest('.input-column').style.paddingTop = '6px'
+        })
+    }
+
+    function addClassCharter () {
+        const section = document.querySelectorAll('section')
+        section.forEach(item => item.classList.add('charter'))
+    }
+
+    document.querySelector('.home-page') ? addClassCharter : null
 });
